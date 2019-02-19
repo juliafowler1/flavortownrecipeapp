85dfd5624f1b4d801108c72eda27c5afce7a4f38
Kathy <kpnelson847@gmail.com>
2019-02-19 08:58:56 -0500

    was able to fetch the data from the recipe site

--- app/components/searchCriteria.component.js
+++ app/components/searchCriteria.component.js
@@ -1,6 +1,17 @@
 "use strict";
 const searchCriteria = {
-    template: `<p>FROM SEARCH CRITERIA</p>`,
+    template: `
+    <p>FROM SEARCH CRITERIA</p>
+    <button ng-click="$ctrl.getApi();">Search</button>
+    `,
+    controller: ["RecipeService", function(RecipeService) {
+        const vm = this;
+        vm.getApi = function() {
+            RecipeService.getredditdata().then(function(response) {
+                console.log(response);
+            })
+        }
+    }]
 }
 
 
--- app/service/app.service.js
+++ app/service/app.service.js
@@ -1,15 +1,16 @@
 "use strict";
 function RecipeService($location, $http) {
     const self = this;
-    // self.getredditdata = function() {
-    //     return $http({
-    //         url: "https://www.reddit.con/r/aww.json",
-    //         method: "GET"
-    //     }).then(function(response){
-    //         self.data-response;
-    //         return self.data;
-    //     })
-    // }
+    self.search = "beef"
+    self.getredditdata = function() {
+        return $http({
+            url: "https://api.edamam.com/search?q="+self.search+"&app_id=8b7bd8f2&app_key=8dc778864de33e6b841e8ebdd5873e87&from=0&to=5&calories=591-722&health=alcohol-free",
+            method: "GET"
+        }).then(function(response){
+            self.data=response;
+            return self.data;
+        })
+    }
 
 }
 
