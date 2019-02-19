dc26fb9334c86b26b3b4cbd4949fc0c01f7c0d46
Kathy <kpnelson847@gmail.com>
2019-02-19 11:11:27 -0500

    got multiple images to display on the page

--- app/components/searchCriteria.component.js
+++ app/components/searchCriteria.component.js
@@ -4,18 +4,27 @@ const searchCriteria = {
     <p>FROM SEARCH CRITERIA</p>
     <input type="text" ng-model="$ctrl.search">
     <button ng-click="$ctrl.getApi();">Search</button>
+    
+
+    <section ng-repeat="items in $ctrl.data.$$state.value.data.hits">
+    <p>{{items.recipe.label}}</p>
+    <img src={{items.recipe.image}}>
+
     `,
     controller: ["RecipeService", function(RecipeService) {
         const vm = this;
         vm.getApi = function() {
-            RecipeService.getredditdata().then(function(response) {
-                console.log(response);
-            })
-        }
+                vm.data = RecipeService.getsearch(vm.search);
+                console.log(vm.data);
+            }
     }]
 }
 
 
+/* <p>{{$ctrl.data.$$state.value.data.hits[0].recipe.label}}</p>
+    <img src={{$ctrl.data.$$state.value.data.hits[0].recipe.image}}>
+    <p>{{$ctrl.data.$$state.value.data.hits[1].recipe.label}}</p>
+    <img src={{$ctrl.data.$$state.value.data.hits[1].recipe.image}}></img> */
 
 angular.module("RecipeApp")
     .component("searchCriteria", searchCriteria);
