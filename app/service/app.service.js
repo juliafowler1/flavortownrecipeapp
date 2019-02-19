6b6714100ed85f7c045f2be953c267c643afc119
Kathy <kpnelson847@gmail.com>
2019-02-19 09:45:52 -0500

    reverted changes I made by accident

--- app/service/app.service.js
+++ app/service/app.service.js
@@ -1,7 +1,7 @@
 "use strict";
 function RecipeService($location, $http) {
     const self = this;
-    self.search = "beef"
+    self.search = search
     self.getredditdata = function() {
         return $http({
             url: "https://api.edamam.com/search?q="+self.search+"&app_id=8b7bd8f2&app_key=8dc778864de33e6b841e8ebdd5873e87&from=0&to=5&calories=591-722&health=alcohol-free",
