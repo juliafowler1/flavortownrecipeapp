23be602997ec498580c8f2172ad67dc5200ec0ea
Kathy <kpnelson847@gmail.com>
2019-02-20 09:26:13 -0500

    added comments to the files

--- app/components/favoritesPage.component.js
+++ app/components/favoritesPage.component.js
@@ -12,18 +12,18 @@ const favoritesPage = {
     controller: ["RecipeService", function(RecipeService) {
         const vm = this;
         // vm.$onInit = function(){ don't think we need this
-            vm.favorites = RecipeService.getFavs();
+            vm.favorites = RecipeService.getFavs(); //set $ctrl.favorites to the data that we received from the service
             console.log(vm.favorites);  //you need to reference vm when you put it in the console
         // }
-        vm.remFavs = function($index) {
-            RecipeService.removeFavs($index);
-            console.log($index);
+        vm.remFavs = function($index) { //when you click on the x button, it will remove the specfic item
+            RecipeService.removeFavs($index); //removes item by the index
+            console.log($index); //just double checking this
             
         }
     }]
     
 }
 
-
+//getter syntax for the component
 angular.module("RecipeApp")
     .component("favoritesPage", favoritesPage);
